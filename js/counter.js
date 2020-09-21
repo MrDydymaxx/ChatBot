6f5e038952c7ee8c8312b0bc6887a677365bbdbf
Alexandra <alexandrapham03@gmail.com>
2020-09-21 17:00:00 +0200

    feat(counter.js): Collect counter value

---
+++ js/counter.js
@@ -0,0 +1,13 @@
+var counterDiablo = document.querySelector(".notification");
+var initialNumberCounter = counterDiablo.textContent;
+console.log(initialNumberCounter);
+
+// counterDiablo.innerHTML = "0";
+// console.log(counterDiablo);
+
+// if {
+// }
+
+
+
+
