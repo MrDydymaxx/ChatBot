ea159506ea32d6eff7dc208b6d20291f7788cdfe
Julia-BARGOIN <julia.bargoin@tuta.io>
2020-09-21 15:16:18 +0200

    feat(bot.js): add json

---
+++ js/bot.js
@@ -0,0 +1,24 @@
+{
+  "answers": [
+    "weather":"il fait chaud",
+    "firstName":"Anna",
+    "lastName":"Smith"
+  ],
+  "picture":"https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/bb21e21f-895d-45d5-b485-30aa061f1b51/d59i9x1-eb1916f1-eb75-4a06-bff3-8753647e09ea.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvYmIyMWUyMWYtODk1ZC00NWQ1LWI0ODUtMzBhYTA2MWYxYjUxXC9kNTlpOXgxLWViMTkxNmYxLWViNzUtNGEwNi1iZmYzLTg3NTM2NDdlMDllYS5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.zSHlP2JZJIK-M-OdipNQ9GKc-xlyTSWLQQ4-I7Z7T0Q"
+},
+{
+  "answers": [
+    "weather":"il fait froid",
+    "firstName":"Rock",
+    "lastName":"Dupon"
+  ],
+  "picture":"https://i.pinimg.com/originals/3a/dd/ca/3addca1ccea8b7c2f69144973a3994b7.jpg"
+},
+{
+  "answers": [
+    "weather":"il fait humide",
+    "firstName":"Meno",
+    "lastName":"Marrin"
+  ],
+  "picture":"https://w0.pngwave.com/png/613/639/counter-strike-global-offensive-counter-strike-source-counter-strike-1-6-video-game-others-png-clip-art-thumbnail.png"
+}
