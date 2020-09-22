07e64aa931ebaf345a9ca9ce1f504744b5512669
Jasmine <jasmine.tanner97@gmail.com>
2020-09-22 10:45:17 +0200

    feat(input): show sent messages

--- js/message.js
+++ js/message.js
@@ -1,4 +1,16 @@
 function myFunction() {
   var message = document.querySelector(".form-control").value;
   console.log(message);
+
+document.querySelector(".conversation").insertAdjacentHTML('beforeend','<div class="d-flex justify-content-end mb-4 new-message">'+
+      		'<div class="msg_container_send">'+
+            	'<span class="msg_name">Panda</span>'+
+            	'<span>'+ message +'</span>'+
+            	'<span class="msg_time">9:20 AM, Today</span>'+
+            '</div>'+
+            '<div class="img_cont_msg">'+
+            	'<img src="https://static.thenounproject.com/png/67331-200.png" class="rounded-circle user_img_msg" />'+
+            '</div>'+
+           '</div>');
+
 }
